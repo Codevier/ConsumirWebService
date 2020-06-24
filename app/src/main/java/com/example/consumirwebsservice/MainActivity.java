16241fe62904671f4a72286b0dc727f4950c3941
Xavviier <jeanxavii1997@gmail.com>
2020-06-24 13:23:43 -0500

    Ejemplo de clase, completo

--- app/src/main/java/com/example/consumirwebsservice/MainActivity.java
+++ app/src/main/java/com/example/consumirwebsservice/MainActivity.java
@@ -6,13 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.consumirwebsservice.WebService.Asynchtask;
+import com.example.consumirwebsservice.WebService.WebService;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements Asynchtask {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Map<String, String> datos = new HashMap<String, String>();
+        WebService ws= new WebService("https://api-uat.kushkipagos.com/transfer-subscriptions/v1/bankList",
+                datos, MainActivity.this, MainActivity.this);
+        ws.execute("GET","Public-Merchant-Id","86fb957359f04d9c8199c865b24cbd96");
     }
     public void Enviar(View view){
         //Creamos el Intent
@@ -27,5 +43,19 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtras(b);
         // Iniciamos la nueva actividad
         startActivity(intent);
+    }
+
+    @Override
+    public void processFinish(String result) throws JSONException {
+        TextView txtSaludo = (TextView)findViewById(R.id.editTextTextMultiLine);
+        //ArrayList<String> lstBancos = new ArrayList<String> ();
+        String listaBancos="";
+        JSONArray JSONlista =  new JSONArray(result);
+        for(int i=0; i< JSONlista.length();i++) {
+            JSONObject banco = JSONlista.getJSONObject(i);
+            listaBancos=listaBancos+banco.getString("name").toString();
+            //lstBancos.add(banco.getString("name").toString());
+        }
+        txtSaludo.setText(listaBancos);
     }
 }
